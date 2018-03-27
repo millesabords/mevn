aafaee22024d356fa3fbabc9eccd3126d1aab90e
millesabords <703804+millesabords@users.noreply.github.com>
2018-03-27 21:43:56 +0200

    testing mongodb insertion ok. Next step: deal with routing which is a mess

---
+++ README.md
@@ -0,0 +1,52 @@
+# mevn-stack
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+#maybe need to cd node_modules and ln -s ../myroutes . and ln -s ../mymodels .
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+following tutorial at: https://www.djamware.com/post/5a1b779f80aca75eadc12d6e/mongo-express-vue-nodejs-mevn-stack-crud-web-application
+- for routes folder, you may have to announce it to npm packaging system:
+ -> $ cd routes; sudo npm link; cd ..; npm link routes; chown -R ouam:ouam /usr/lib/node_modules/routes; remove package-lock and add "routes":"*" in package.json dependancies section and redo npm install
+- for mogoose: remove 'useMongoClient' directive in app.js
+- for mongodb related stuff:
+maybe modify mongo stuff: authentication, ssl, host/port: mongo --host 127.0.0.1:27017
+
+start mongodb (server) first and then mongo (CLI client for init and tests...):
+$ chmod g+w /var/lib/mongodb (before )
+$ sudo passwd mongodb ... // change mongodb user password
+$ sudo usermod -a -G mongodb ouam ...// add mongodb group to me
+$ sudo chmod g+w /var/log/mongodb/mongodb.log
+$ sudo chmod g+w /var/log/mongodb
+check that dbpath points to '/var/lib/mongodb' in /etc/mongodb.conf file and then:
+$ sudo service mongodb restart
+$ mongo
+use mven; //will create empty database 'mevn'
+db.createUser({user: "mynewuser", pwd: "myuser123", roles: [ "readWrite", "dbAdmin" ] });
+db.movie.insert({"name":"whatever"})
+
---
+++ app.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var app = express();
+
+//path.resolve(__dirname, '../dist/index.html')
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+
+/* test */
+var BookSchema = require('mymodels');
+//var router = express.Router();
+//router.get('/', function(req, res, next) {
+app.use(function(req, res, next) {
+//  res.send('Wiki home page');
+  console.log("entering test route....");
+var test1 = new BookSchema({title:'nonameahaha'});
+  test1.save(function (err,test1) {
+    if (err) return console.error(err);
+    console.log("saving book with name: " + test1.title);
+  });
+/*test1.find(function (err,test1) {
+  if (err) return console.error(err);
+  console.log("finding any book in collection: " + test1);
+})*/
+  res.send('Express RESTful API');
+//Book.find(function (err, products) {
+//    res.json(products);
+//  });
+
+});
+/* end of test */
+
+/*
+var book = require('myroutes');
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/books', express.static(path.join(__dirname, 'dist')));
+app.use('/book', book);
+*/
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/*
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  //res.send('error' + err.message);
+  res.render('error');//todo la vue erreur n'existe pas
+});
+
+
+app.set('views', path.join(__dirname, 'dist'));
+//app.set('views', __dirname + '/views');
+//app.use(express.static(path.join(__dirname, '/public')));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+*/
+
+//import mongoose from 'mongoose';
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost/mevn', { promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+// mongoose.on('error', console.error.bind(console, 'connection error:'));
+
+module.exports = app;
+
